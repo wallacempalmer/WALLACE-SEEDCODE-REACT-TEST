1c7c4bf6afcb88dc467771570925dab324f8f672
Eric Ettensohn <ericettensohn@gmail.com>
2021-07-21 07:54:38 -0700

    language

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,11 +3,7 @@ export const Home = () => {
     <div>
       <h2>Welcome!</h2>
       <p>
-        Complete tests 1, 2, and 3 in any order. Types for each component have
-        been provided so you know what to expect, but feel free to ignore
-        Typescript if you're not familiar with it. If you'd like Typescript
-        linting to be enabled, remove the TSlint disable rule at the top of each
-        file. When you're done, commit the changes to a new branch with your
+        Complete tests 1, 2, and 3 in any order. When you're done, commit the changes to a new branch with your
         first name.
       </p>
     </div>
